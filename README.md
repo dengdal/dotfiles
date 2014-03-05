45aa67f7e6225933f71c6b9939606d6e8af82e22
David Engdal <david.engdal@ericsson.com>
2014-03-05 13:00:49 +0100

    working install.sh

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ dotfiles
 ========
 
 For all my dotfiles
+
+To install, just run:
+install.sh
+
+/David Engdal, 2014-03-04
---
+++ install.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+move_and_link() {
+FILE=$1
+LINK=$2
+
+if [ -f $LINK ]; # backup if a file
+then
+	mv -f $LINK $LINK.old
+	echo "Old file $LINK moved to $LINK.old";
+fi
+ln -sf $FILE $LINK
+}
+
+cd
+move_and_link .dotfiles/.vim/vimrc .vimrc
+move_and_link .dotfiles/dotgit/.gitignore_global .gitignore_global
+git config --global core.excludesfile .gitignore_global
