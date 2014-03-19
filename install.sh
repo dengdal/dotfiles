4786020588b04ed7d7a552136017a3d2b089b867
David Engdal <david.engdal@gmail.com>
2014-03-19 13:12:56 +0100

    now w ok cscope and ctags support and dotfiles for bash incl aliases and some old scripts

--- install.sh
+++ install.sh
@@ -16,4 +16,6 @@ cd
 move_and_link .dotfiles/.vim/vimrc .vimrc
 move_and_link .dotfiles/.vim .vim
 move_and_link .dotfiles/dotgit/.gitignore_global .gitignore_global
+move_and_link .dotfiles/dotbash/.bash_aliases .bash_aliases
+cat .dotfiles/dotbash/.bashrc.user >> .bashrc
 git config --global core.excludesfile .gitignore_global
