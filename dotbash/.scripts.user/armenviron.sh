156ff3820b86fcd14a59863aa08eabf17566afbf
David Engdal <david.engdal@ericsson.com>
2014-11-13 10:10:44 +0100

    some adds specific for my work environment

---
+++ dotbash/.scripts.user/armenviron.sh
@@ -0,0 +1,16 @@
+#
+# emwdea armenviron.sh
+#
+#export ARMLMD_LICENSE_FILE=<mylicense_file>@<mylicense_server>
+export ARMLMD_LICENSE_FILE=5223@armlic01.ld.sw.ericsson.se
+export LM_LICENSE_FILE=5223@armlic01.ld.sw.ericsson.se
+export ARMTOOLS=ARMCT5.01
+export ARMROOT=/home/emwdea/ARM_Compiler_5
+export ARM_COMPILER_PATH=$ARMROOT/bin64
+export ARMLIB=$ARMROOT/lib
+export ARMINCLUDE=$ARMROOT/include
+export ARMINC=$ARMINCLUDE
+export ARMBIN=$ARMROOT/bin64
+export PATH=$ARM_COMPILER_PATH:$PATH
+export ARMHOME=$ARMROOT
+
